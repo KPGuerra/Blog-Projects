f2f149b5f04717a24cf452c56dfadce57a38ceb9
KPGuerra <KPGuerra96@gmail.com>
2021-05-15 17:22:52 -0400

    Example 1 Done

---
+++ svg-pros-and-cons/src/Components/HomePage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import DragonBall from './DragonBall.svg'
+
+function HomePage () {
+    return (
+        <div>
+            <h1> Welcome! </h1>
+            <img src = {DragonBall} alt="SVG"/>
+        </div>
+    )
+}
+
+export default HomePage
+
+
+
+
+
+
+
+
