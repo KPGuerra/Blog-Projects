c99e2c53dc78e81cd96c37081ec06a3d1839dcea
KPGuerra <KPGuerra96@gmail.com>
2021-05-15 17:36:15 -0400

    Example 2 Done

--- svg-pros-and-cons/src/Components/HomePage.jsx
+++ svg-pros-and-cons/src/Components/HomePage.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import DragonBall from './DragonBall.svg'
+// import DragonBall from './DragonBall.svg'
 
-function HomePage () {
+function HomePage() {
     return (
         <div>
             <h1> Welcome! </h1>
-            <img src = {DragonBall} alt="SVG"/>
+            <svg width="182" height="178" viewBox="0 0 182 178" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <ellipse cx="91" cy="89" rx="91" ry="89" fill="#F8921B" />
+                <path d="M89.5 33L92.7555 41.9828H103.29L94.7674 47.5344L98.0229 56.5172L89.5 50.9656L80.9771 56.5172L84.2326 47.5344L75.7097 41.9828H86.2445L89.5 33Z" fill="#EE0808" />
+                <path d="M118.5 76L121.755 84.9828H132.29L123.767 90.5344L127.023 99.5172L118.5 93.9656L109.977 99.5172L113.233 90.5344L104.71 84.9828H115.245L118.5 76Z" fill="#EE0808" />
+                <path d="M66.5 76L69.7555 84.9828H80.2903L71.7674 90.5344L75.0229 99.5172L66.5 93.9656L57.9771 99.5172L61.2326 90.5344L52.7097 84.9828H63.2445L66.5 76Z" fill="#EE0808" />
+            </svg>
         </div>
     )
 }
